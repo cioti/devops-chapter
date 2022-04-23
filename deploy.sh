dd4ed7e6e0285e1dc367b68f81686470684af58f
cioti <alex.ciotirla@gmail.com>
2022-04-23 19:20:29 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ echo "token is ${GH_SVC_ACCOUNT_TOKEN}"
 echo "token2 is ${TOKEN2}"
 curl --request POST \
     --url https://api.github.com/repos/cioti/devops-chapter/pulls \
-    --header 'authorization: Bearer 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' \
+    --header 'authorization: Bearer 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' \
     --header 'content-type: application/json' \
     --data '{"title":"test","body":"Please pull these awesome changes in!","head":"cioti:test-ci","base":"main"}'
 }
