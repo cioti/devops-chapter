e3bbfb9d0dca0b17c1b6e17e3e901b62c83317d5
cioti <alex.ciotirla@gmail.com>
2022-04-23 13:22:56 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,8 @@ svc_name=$1
 echo "Service processed is $svc_name"
 values_path=`echo $APPS_PATH/$svc_name/values.yaml`
 
-echo "Values path=$values_path"
 if [ -f $values_path ]; then  
-    echo "values file exists"
+    exit 1
 fi
 git config --global user.email "service@account.net"
 git config --global user.name "service_account"
