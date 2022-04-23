1eb51e09f445c956dfcc36dee70dfeefa0d30233
cioti <alex.ciotirla@gmail.com>
2022-04-23 14:41:25 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,26 @@
 
 SERVICES=$1
 APPS_PATH="apps/staging"
+pull_request() {
+  to_branch=$1
+  if [ -z $to_branch ]; then
+    to_branch="main"
+  fi
+  
+  # try the upstream branch if possible, otherwise origin will do
+  upstream=$(git config --get remote.upstream.url)
+  origin=$(git config --get remote.origin.url)
+  if [ -z $upstream ]; then
+    upstream=$origin
+  fi
+  
+  to_user=$(echo $upstream | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
+  from_user=$(echo $origin | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
+  repo=$(basename `git rev-parse --show-toplevel`)
+  from_branch=$(git rev-parse --abbrev-ref HEAD)
+  open "https://github.com/$to_user/$repo/pull/new/$to_user:$to_branch...$from_user:$from_branch"
+}
+
 
 createPullRequest(){
 svc_name=$1
@@ -18,9 +38,11 @@ yq e '.spec.chart.spec.version = "1.9.9"' -i $values_path
 git add -A $values_path
 git commit -m "test"
 git push -u origin deploy-$svc_name-1.9.9
-echo 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 > .githubtoken
-gh auth login --with-token < .githubtoken
-gh pr create --title "The bug is fixed" --body "Everything works again"
+
+pull_request
+# echo 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 > .githubtoken
+# gh auth login --with-token < .githubtoken
+# gh pr create --title "The bug is fixed" --body "Everything works again"
 }
 
 # split services by ','
